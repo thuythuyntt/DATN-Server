ab9d8e680c6a173d225ff467250dd7713ea84d4a
THUY <thuthuynt.0710@gmail.com>
2018-11-17 16:53:50 +0700

    no message

--- src/main/java/server/WebSocketServer.java
+++ src/main/java/server/WebSocketServer.java
@@ -10,78 +10,53 @@ import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WebSocketServer {
-	
-	private int port;
-	private String webSocketPath;
-	
-	private boolean ssl = System.getProperty("ssl") != null;
-	private SslContext sslCtx;
-	
-    public WebSocketServer(int port, String webSocketPath) {
-    	this.setPort(port);
-    	this.setWebSocketPath(webSocketPath);
-    }
-    
-    public void run() throws Exception {
-    	// Configure SSL.
-        if (ssl) {
-            SelfSignedCertificate ssc = new SelfSignedCertificate();
-            sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
-        } else {
-            sslCtx = null;
-        }
 
+    private int port;
+    private String webSocketPath;
+
+    public WebSocketServer(int port, String webSocketPath) {
+        this.setPort(port);
+        this.setWebSocketPath(webSocketPath);
+    }
+
+    public void run() {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
-             .channel(NioServerSocketChannel.class)
-             .handler(new LoggingHandler(LogLevel.INFO))
-             .childHandler(new WebSocketServerChannelInitializer(this));
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new WebSocketServerChannelInitializer(this));
 
             Channel ch = b.bind(port).sync().channel();
-
-            // test via browser also possible
-            // System.out.println("Open your web browser and navigate to " + (ssl? "https" : "http") + "://127.0.0.1:" + port + '/');
-
             ch.closeFuture().sync();
+
+        } catch (InterruptedException ex) {
+            Logger.getLogger(WebSocketServer.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
     }
-    
+
     public int getPort() {
-    	return port;
+        return port;
     }
-    
+
     public void setPort(int port) {
-    	this.port = port;
+        this.port = port;
     }
     
-    public boolean isSsl() {
-    	return ssl;
+    public String getWebSocketPath() {
+        return webSocketPath;
     }
-    
-    public SslContext getSslContext() {
-    	return this.sslCtx;
-    }
-    
-    public void setSslContext(SslContext sslCtx) {
-    	this.sslCtx = sslCtx;
-    }
-  
-	public String getWebSocketPath() {
-		return webSocketPath;
-	}
 
-	public void setWebSocketPath(String webSocketPath) {
-		this.webSocketPath = webSocketPath;
-	}
-
-    
+    public void setWebSocketPath(String webSocketPath) {
+        this.webSocketPath = webSocketPath;
+    }
 }
